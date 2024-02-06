5bbe2afc2d1af25ea1aa1b7cdfebfdc7024a69c8
Oleksii Hryhorenko <aleksey2892@gmail.com>
2024-02-06 18:11:07 +0200

    add AuthLayout

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from './components/MainLayout/MainLayout.jsx';
 import HomePage from './pages/HomePage/HomePage.jsx';
+import ProductsPage from './pages/ProductsPage/ProductsPage.jsx';
+
+import AuthLayout from './components/AuthLayout/AuthLayout.jsx';
 import RegisterPage from './pages/RegisterPage/RegisterPage.jsx';
+import LoginPage from './pages/LoginPage/LoginPage.jsx';
+
 import FormTest from './components/FormForTest/FormTest.jsx';
 import FormForTestWithImage from './components/FormForTestWithImage/FormForTestWithImage.jsx';
-import ProductsPage from './pages/ProductsPage/ProductsPage.jsx';
-import AuthPage from './pages/AuthPage/AuthPage.jsx';
 
 const App = () => {
   return (
@@ -17,8 +20,10 @@ const App = () => {
           <Route path="/category/:name" element={<ProductsPage />} />
         </Route>
 
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="" element={<AuthLayout />}>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
 
         <Route path="/form" element={<FormTest />} />
         <Route path="/formImage" element={<FormForTestWithImage />} />
--- src/components/AuthLayout/AuthLayout.styled.js
+++ src/components/AuthLayout/AuthLayout.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const AuthPageContainer = styled.div`
+export const MainContainer = styled.main`
   width: 100%;
   height: 100vh;
   display: flex;
