4305d2850319b17127c79838cab98195e8076e3f
Oleksii Hryhorenko <aleksey2892@gmail.com>
2024-03-13 22:46:15 +0200

    fix baseURL

--- src/api/axiosAPI.js
+++ src/api/axiosAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const mainInstance = axios.create({
-  baseURL: 'https://api.oranger.store/api/',
+  baseURL: 'https://api.oranger.store/',
 });
 
 export default mainInstance;
